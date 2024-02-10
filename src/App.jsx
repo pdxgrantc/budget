482f3524f377baf6a5803830a253f604d448ae14
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-02-10 12:13:01 -0800

    Remove email logo from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { useAuthState } from "react-firebase-hooks/auth";
 // icons
 import { SiGithub as GitHubLogo } from "react-icons/si";
 import { FaLinkedinIn as LinkedLogo } from "react-icons/fa";
-import { IoIosMail as MailLogo } from "react-icons/io";
 import { IoPersonCircleSharp as AboutLogo } from "react-icons/io5";
 
 // Images
@@ -103,9 +102,6 @@ function Root(props) {
           <div className="flex gap-6 w-fit mx-auto">
             <OutsideLink link="https://www.github.com/pdxgrantc">
               <GitHubLogo className="h-[4rem] w-auto py-2 mx--auto text-button hover:text-button_hover" />
-            </OutsideLink>
-            <OutsideLink link="mailto:pdxgrantc@gmail.com">
-              <MailLogo className="h-[4rem] py-0 w-auto text-button hover:text-button_hover" />
             </OutsideLink>
             <OutsideLink link="https://pdxgrantc.com/">
               <AboutLogo className="h-[3.5rem] w-auto text-button hover:text-button_hover" />
